9252f8e2b6492677c9f86268d4f07962822d2746
RedSparr0w <admin@redsparr0w.com>
2019-08-25 13:40:29 +1200

    [Shop] Mr. Mime

--- src/scripts/GameConstants.ts
+++ src/scripts/GameConstants.ts
@@ -527,7 +527,7 @@ namespace GameConstants {
         "Eevee": 5000,
         "Porygon": 2000,
         "Jynx": 2500,
-        "Mr_Mime": 1500,
+        "Mr. Mime": 1500,
         "Lickitung": 1000,
         "Togepi": 2500,
 
@@ -567,7 +567,7 @@ namespace GameConstants {
         "Eevee",
         "Porygon",
         "Jynx",
-        "Mr_Mime",
+        "Mr. Mime",
         "Lickitung",
         "Togepi",
     }
