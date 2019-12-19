6d922409e2ca671279f2d5154297be0b353f408e
RedSparr0w <admin@redsparr0w.com>
2019-12-20 12:47:26 +1300

    Quest slots unlocked base on quest level

--- src/scripts/quests/QuestHelper.ts
+++ src/scripts/quests/QuestHelper.ts
@@ -197,7 +197,8 @@ class QuestHelper{
 
     public static questSlots(): KnockoutComputed<number> {
         return ko.computed(function () {
-            return player ? player.highestRegion() + 1 : 1;
+            // Minimum of 1, Maximum of 4
+            return Math.min(4, Math.max(1, player ? Math.floor(player.questLevel / 5) : 1));
         }, this);
     }
 }
